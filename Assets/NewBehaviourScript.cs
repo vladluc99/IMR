60584700e6dbebe236f3dcb100ef15bacd6959e9
vladluc99 <62203002+vladluc99@users.noreply.github.com>
2020-10-07 17:35:02 +0300

    Hello World

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    
+    
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GetComponent<Transform>().position.y >13)
+            GetComponent<Renderer>().material.color = Color.red;
+        if (GetComponent<Transform>().position.y <13)
+            GetComponent<Renderer>().material.color = Color.blue;
+    }
+}
